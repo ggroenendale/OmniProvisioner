e27e8996d005fe34a35a0b53f72fa20ab4588624
Geoff Groenendale <g.groenendale@gmail.com>
2025-06-26 21:38:05 -0700

    Added steps to make the archiso and added config variables

--- scripts/arch/copy_files_to_newiso.sh
+++ scripts/arch/copy_files_to_newiso.sh
@@ -5,6 +5,12 @@
 # next boot with the newly packaged iso, arch linux will install and run all of our installation
 # scripts
 
+# Configs
+OUTPUT_DIR=~/archiso-out
+PROFILE_DIR=~/new-iso
+CUSTOM_NAME=latest-custom-archlinux.iso
+VENTOY_MOUNT=/mnt/ventoy
+
 # First make sure that the archiso package is installed
 pacman -S archiso
 
@@ -18,8 +24,8 @@ cd ~/new-iso
 #   lsblk -f  #Find the USB device name i.e. /dev/sdb
 #
 VENTOY_DEV="/dev/disk/by-id/usb-PNY_USB_3.1_FD_0700150D9DADBA90-0:0-part1"
-mkdir -p /mnt/ventoy
-mount -t vfat -o rw,umask=000 "${VENTOY_DEV}-part1" /mnt/ventoy
+mkdir -p "$VENTOY_MOUNT"
+mount -t vfat -o rw,umask=000 "${VENTOY_DEV}-part1" "$VENTOY_MOUNT"
 
 # Verify Ventoy Mount
 echo "Checking /mnt/ventoy mount..."
@@ -34,10 +40,19 @@ else
 fi
 
 # Copy the install script, python config, json config, and vault_pass to the airootfs/root/ folder
-cp -r /mnt/ventoy/scripts/arch/arch_install.sh ~/new-iso/airootfs/root/
-cp -r /mnt/ventoy/scripts/arch/install_arch.py ~/new-iso/airootfs/root/
-cp -r /mnt/ventoy/scripts/arch/archconfig.json ~/new-iso/airootfs/root/
-cp -r /mnt/ventoy/.vault_pass.txt ~/new-iso/airootfs/root/
+cp -r "$VENTOY_MOUNT/scripts/arch/arch_install.sh" "$PROFILE_DIR/airootfs/root/"
+cp -r "$VENTOY_MOUNT/scripts/arch/install_arch.py" "$PROFILE_DIR/airootfs/root/"
+cp -r "$VENTOY_MOUNT/scripts/arch/archconfig.json" "$PROFILE_DIR/airootfs/root/"
+cp -r "$VENTOY_MOUNT/.vault_pass.txt" "$PROFILE_DIR/airootfs/root/"
 
-# Once these files are inside the airootfs then we should be able to repackage the iso and then reboot...
+# Once these files are inside the airootfs then we should be able to repackage the iso
+mkarchiso -v -o "$OUTPUT_DIR" "$PROFILE_DIR"
 
+# Find the freshly built ISO
+ISO=$(ls "$OUTPUT_DIR"/*.iso)
+
+# Rename it
+mv "$ISO" "$OUTPUT_DIR/$CUSTOM_NAME"
+
+# Finally, copy the new-iso back onto the ventoy usb so it can be used in the next load.
+cp "$OUTPUT_DIR/$CUSTOM_NAME" "$VENTOY_MOUNT/isos/Arch/"
