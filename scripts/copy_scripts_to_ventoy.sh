c2c9a0547fdba0f0e7ff9c57f7fa5e7d24227eb8
Geoff Groenendale <g.groenendale@gmail.com>
2025-07-04 04:03:19 -0700

    Added a simple script to simplify copying updated script files to Ventoy USB

---
+++ scripts/copy_scripts_to_ventoy.sh
@@ -0,0 +1,11 @@
+# Copy Arch Linux scripts to Ventoy
+cp -r scripts/arch/arch_install.sh /media/geoff/Ventoy/scripts/arch/arch_install.sh
+cp -r scripts/arch/archinstall-configure-newiso.sh /media/geoff/Ventoy/scripts/arch/archinstall-configure-newiso.sh
+
+# Copy Debian scripts to Ventoy
+cp -r scripts/debian/debian_install.sh /media/geoff/Ventoy/scripts/debian/debian_install.sh
+cp -r scripts/debian/preseed.cfg /media/geoff/Ventoy/scripts/debian/preseed.cfg
+cp -r scripts/debian/postinstall.service /media/geoff/Ventoy/scripts/debian/postinstall.service
+
+# Copy Raspberry Pi scripts to Ventoy
+cp -r scripts/raspberrypi/raspberrypi_install.sh /media/geoff/Ventoy/scripts/raspberrypi/raspberrypi_install.sh
